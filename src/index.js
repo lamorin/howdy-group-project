c849708cdf57c14b27d35e40637e606558ee704b
lamorin <luis.morin@gmail.com>
2020-05-23 21:42:48 -0400

    Configuration Firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 // import red from "@material-ui/core/colors/red";
 
 // import "./index.css";
-import App from "./App";
+import App from './App'
 
 const theme = createMuiTheme({
   // palette: {
   //   primary: red
-  // }  
-});
+  // }
+})
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+}
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
@@ -19,8 +28,8 @@ ReactDOM.render(
       <App />
     </Router>
   </MuiThemeProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
