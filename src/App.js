8855c52bd374303c5a62ae08500837bdee659b59
lamorin <luis.morin@gmail.com>
2020-05-23 22:44:49 -0400

    Firestore configuration working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useState } from 'react'
+import { Route } from 'react-router-dom'
 // import styled from "styled-components";
+import firebase from 'firebase'
+//import firebase from 'firebase/app'
+import 'firebase/database'
 
 //comment data may not need to be passed down to rightpanel.
-import { comments, postData } from "./data/store.js";
+import { comments, postData } from './data/store.js'
 
-import CombinedPanels from "./components/Forum/Combined-Panels";
+import CombinedPanels from './components/Forum/Combined-Panels'
 
-import PostDetail from "./components/Dialogs/PostDetail.js";
+import PostDetail from './components/Dialogs/PostDetail.js'
 // import PostReply from "./components/Dialogs/PostReply.js";
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBd1kYqf8TCSKdgeXDqsG1WE5M_zsMPqY8',
+  authDomain: 'howdy-group-project.firebaseapp.com',
+  databaseURL: 'https://howdy-group-project.firebaseio.com',
+  projectId: 'howdy-group-project',
+  storageBucket: 'howdy-group-project.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: '1:749459702509:web:e702e139af93341cc2c2e4',
+  measurementId: 'G-measurement-id'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+db.collection('users')
+  .add({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  })
+  .then(function (docRef) {
+    console.log('Document written with ID: ', docRef.id)
+  })
+  .catch(function (error) {
+    console.error('Error adding document: ', error)
+  })
 
 // import "./App.css";
 
-function App() {
+function App () {
   //NEW POST CONTROLLERS
 
   /*****POST CONTROLLER LOGIC*********/
@@ -20,46 +52,46 @@ function App() {
   //Initial formstate is for setting current post. This is then updated basedon the post that the user wants to edit.
   const initialFormState = {
     id: null,
-    title: "",
+    title: '',
     name: null,
-    postText: "",
+    postText: '',
     comment: null,
     flag: null,
     date: null,
     time: null
-  };
+  }
 
   //sets inital list of posts that are stored in db (currently dummy data in store.js)
-  const [userPosts, setUserPosts] = useState(postData);
+  const [userPosts, setUserPosts] = useState(postData)
   //currentp post is used for editing functionality.  basically once
-  const [currentPost, setCurrentPost] = useState(initialFormState);
+  const [currentPost, setCurrentPost] = useState(initialFormState)
   //editing switch.  HOwever, this is not used as newpost and edit existing forms are in seperate pages.
-  const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(false)
 
   //logic for adding new post.
   const addPost = newPost => {
-    newPost.id = userPosts.length + 1;
-    const newPosts = [...userPosts, newPost];
-    console.log("App.js - addPost - newPosts updated", newPosts);
-    setUserPosts(newPosts);
+    newPost.id = userPosts.length + 1
+    const newPosts = [...userPosts, newPost]
+    console.log('App.js - addPost - newPosts updated', newPosts)
+    setUserPosts(newPosts)
     // setUserPost(initialFormState);
-  };
+  }
 
   //logic for deleting post.
   const deletePost = id => {
-    setEditing(false);
-    setUserPosts(userPosts.filter(post => post.id !== id));
-  };
+    setEditing(false)
+    setUserPosts(userPosts.filter(post => post.id !== id))
+  }
 
   //logic for updating a post once it's edited.
   const updatePost = (id, updatedPost) => {
-    setEditing(false);
-    setUserPosts(userPosts.map(post => (post.id === id ? updatedPost : post)));
-  };
+    setEditing(false)
+    setUserPosts(userPosts.map(post => (post.id === id ? updatedPost : post)))
+  }
 
   //edits existin post.
   const editPost = post => {
-    setEditing(true);
+    setEditing(true)
 
     setCurrentPost({
       id: post.id,
@@ -70,17 +102,31 @@ function App() {
       flag: post.flag,
       date: post.date,
       time: post.time
-    });
-  };
+    })
+  }
 
   /*****REPLY CONTROLLER LOGICSHOULD GO HERE BUT CURRENTLY IN POSTDETAIL.JS; NEEDS TO BE RE-FACTORED.*********/
 
-  
+  // Add a new document in collection "cities"
+  db.collection('cities')
+    .doc('LA')
+    .set({
+      name: 'Los Angeles',
+      state: 'CA',
+      country: 'USA'
+    })
+    .then(function () {
+      console.log('Document successfully written!')
+    })
+    .catch(function (error) {
+      console.error('Error writing document: ', error)
+    })
+
   return (
-    <div className="App">
+    <div className='App'>
       <Route
         exact
-        path="/"
+        path='/'
         render={props => (
           <CombinedPanels
             {...props}
@@ -104,13 +150,13 @@ function App() {
         )}
       />
       <Route
-        path="/posts/:id"
+        path='/posts/:id'
         render={props => (
           <PostDetail {...props} comments={comments} post={postData} />
         )}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
 // import red from "@material-ui/core/colors/red";
 
 // import "./index.css";
@@ -12,15 +13,6 @@ const theme = createMuiTheme({
   //   primary: red
   // }
 })
-
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
-}
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
